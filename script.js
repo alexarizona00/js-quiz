38f9b180780a161beb753c915528304ba72f653a
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-20 21:01:14 -0700

    added more logic for moving through the questions and trying to keep score

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let questions = ["what is this", "what is that", "how many this","how many that","what is the answer to everything"];
+let questions = ["what is this", "what is that", "how many this", "how many that", "what is the answer to everything"];
 let correctAnswers = ['something', 'something else', 'a litte', 'a lottle', '42'];
-let incorrectAnswers = ['123','321','111','666','420','710','911','711','411','*69'];
+let incorrectAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69'];
 let startButton = document.querySelector("#pressme");
 let choiceA = document.querySelector("#a");
 let choiceB = document.querySelector("#b");
@@ -8,38 +8,78 @@ let choiceC = document.querySelector("#c");
 let choiceD = document.querySelector("#d");
 let questionElem = document.querySelector("#question")
 let headerElem = document.querySelector('.main-title')
-headerElem.setAttribute('game-state','pregame')
+headerElem.setAttribute('game-state', 'pregame')
+let correctcount = 0;
+let wrongcount = 0;
+currentQuestion = 0;
+
 
 headerCheck();
 
-function startQuiz(){
-    headerElem.setAttribute('game-state','gametime')
+// starts quiz, changes header, displays first question
+
+function startQuiz() {
+    headerElem.setAttribute('game-state', 'gametime')
     headerCheck()
+    questionElem.textContent = questions[1]
+
     console.log('quiz started')
+
 }
 
-function headerCheck(){
 
-    if (headerElem.getAttribute('game-state') == 'pregame'){
+
+choiceA.addEventListener('click', function(){
+    questionElem.textContent = questions[currentQuestion++]
+    nextQ()
+})
+choiceB.addEventListener('click', function(){
+    questionElem.textContent = questions[currentQuestion++]
+    nextQ()
+})
+choiceC.addEventListener('click', function(){
+    questionElem.textContent = questions[currentQuestion++]
+    nextQ()
+})
+choiceD.addEventListener('click', function(){
+    questionElem.textContent = questions[currentQuestion++]
+    nextQ()
+})
+
+
+
+// checks to see the game state and which header to display
+function headerCheck() {
+
+    if (headerElem.getAttribute('game-state') == 'pregame') {
         headerElem.textContent = "Welcome to Alex's quiz!"
     }
-    else if (headerElem.getAttribute('game-state') == 'gametime'){
+    else if (headerElem.getAttribute('game-state') == 'gametime') {
         headerElem.textContent = ""
-}
-else if (headerElem.getAttribute('game-state') == 'postgame')
-{
-    headerElem.textContent = "Game over! Let's see how you did"
-}
+    }
+    else if (headerElem.getAttribute('game-state') == 'postgame') {
+        headerElem.textContent = "Game over! Let's see how you did"
+    }
 }
 
-function nextQ(){
+
+//runs logic to load next question from array.
+function nextQ() {
+
+    
     console.log("time for the next question")
 }
 
-function gameOver(){
+function gameOver() {
+    headerElem.setAttribute('game-state', 'postgame')
 
     console.log('the game has ended')
 }
 
 startButton.addEventListener('click', startQuiz)
 
+
+
+if (questions[6])[
+    console.log('6 worked')
+]
