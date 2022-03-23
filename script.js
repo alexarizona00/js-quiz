8fd536c253a33ad90ced22e78c6ef7226d7acb28
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-22 17:25:55 -0700

    almost finished with logic on how to determine a right answer and how to credit a person if the answer is right / wrong

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let questions = ["what is this", "what is that", "how many this", "how many that", "what is the answer to everything"];
 let correctAnswers = ['something', 'something else', 'a litte', 'a lottle', '42'];
 let incorrectAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69'];
+let allAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69','something', 'something else', 'a litte', 'a lottle', '42' ]
 let startButton = document.querySelector("#pressme");
 let choiceA = document.querySelector("#a");
 let choiceB = document.querySelector("#b");
@@ -11,9 +12,11 @@ let headerElem = document.querySelector('.main-title')
 headerElem.setAttribute('game-state', 'pregame')
 let correctcount = 0;
 let wrongcount = 0;
-currentQuestion = 0;
-let timeLeft = 59;
+let currentQuestion = 0;
+let timeLeft = 5;
 let timerCountEl = document.querySelector("#thetimer");
+let alltheButtons = document.querySelector('.choices');
+let selectedAnswer = '';
 
 
 headerCheck();
@@ -23,32 +26,42 @@ headerCheck();
 function startQuiz() {
     headerElem.setAttribute('game-state', 'gametime')
     headerCheck()
+    startButton.setAttribute('style', 'display: none')
     timerGo()
     questionElem.textContent = questions[1]
+    alltheButtons.setAttribute('style', 'display: flex')
 
     console.log('quiz started')
 
 }
-
-
-
-choiceA.addEventListener('click', function(){
+choiceA.addEventListener('click', function () {
     questionElem.textContent = questions[currentQuestion++]
     nextQ()
 })
-choiceB.addEventListener('click', function(){
+choiceB.addEventListener('click', function () {
     questionElem.textContent = questions[currentQuestion++]
     nextQ()
 })
-choiceC.addEventListener('click', function(){
+choiceC.addEventListener('click', function () {
     questionElem.textContent = questions[currentQuestion++]
     nextQ()
 })
-choiceD.addEventListener('click', function(){
+choiceD.addEventListener('click', function () {
     questionElem.textContent = questions[currentQuestion++]
     nextQ()
 })
 
+
+if (correctAnswers.includes(selectedAnswer){
+    correctcount++
+}
+    else{
+        wrongcount++
+
+    }
+
+
+//)
 
 
 // checks to see the game state and which header to display
@@ -69,7 +82,7 @@ function headerCheck() {
 //runs logic to load next question from array.
 function nextQ() {
 
-    
+
     console.log("time for the next question")
 }
 
@@ -79,20 +92,20 @@ function gameOver() {
     console.log('the game has ended')
 }
 
-function timerGo(){
-    startButton.setAttribute('style', 'display: none')
-    let timerInterval = setInterval(function(){
-timeLeft--;
-timerCountEl.textContent = timeLeft + " seconds remaining"
-if (timeLeft == 1)
-timerCountEl.textContent = timeLeft + " second remaining"
-if (timeLeft == 0){
-    clearInterval(timerInterval);
-    timerCountEl.textContent = "Times up!"
-    headerElem.setAttribute('game-state', 'postgame')
-    }
+function timerGo() {
+    let timerInterval = setInterval(function () {
+        timeLeft--;
+        timerCountEl.textContent = timeLeft + " seconds remaining"
+        if (timeLeft == 1)
+            timerCountEl.textContent = timeLeft + " second remaining"
+        if (timeLeft == 0) {
+            clearInterval(timerInterval);
+            timerCountEl.textContent = "Times up!"
+            headerElem.setAttribute('game-state', 'postgame')
+            headerCheck()
+        }
 
-}, 1000)
+    }, 1000)
 }
 
 startButton.addEventListener('click', startQuiz)
