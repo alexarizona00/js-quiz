f884a6e0d1b64e49e4e07d216ae309e7c095e051
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-23 08:14:45 -0700

    made changes to buttons and to the questions formatting to make my method of anser verification possible

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let questions = ["what is this", "what is that", "how many this", "how many that", "what is the answer to everything"];
-let correctAnswers = ['something', 'something else', 'a litte', 'a lottle', '42'];
+let questions = ["What is the hyper text markup language?", "this is question number two", "This is question number 3", "this is question number 4", "what is the answer to everything"];
+let correctAnswers1 = ['correct', 'something else', 'a litte', 'a lottle', '42'];
+let correctAnswers2 = ['wrong', 'correct', 'a litte', 'a lottle', '42'];
+let correctAnswers3 = ['wrong', 'something else', 'correct', 'a lottle', '42'];
+let correctAnswers4 = ['wrong', 'something else', 'a litte', 'correct', '42'];
+let correctAnswers5 = ['wrong', 'something else', 'a litte', 'a lottle', 'correct'];
 let incorrectAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69'];
-let allAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69','something', 'something else', 'a litte', 'a lottle', '42' ]
+let allAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69', 'something', 'something else', 'a litte', 'a lottle', '42']
 let startButton = document.querySelector("#pressme");
 let choiceA = document.querySelector("#a");
 let choiceB = document.querySelector("#b");
@@ -13,10 +17,12 @@ headerElem.setAttribute('game-state', 'pregame')
 let correctcount = 0;
 let wrongcount = 0;
 let currentQuestion = 0;
-let timeLeft = 60;
+let timeLeft = 3;
 let timerCountEl = document.querySelector("#thetimer");
 let alltheButtons = document.querySelector('.choices');
 let selectedAnswer = '';
+let testvar = 0;
+let timerInterval = '';
 
 
 headerCheck();
@@ -28,12 +34,21 @@ function startQuiz() {
     headerCheck()
     startButton.setAttribute('style', 'display: none')
     timerGo()
-    questionElem.textContent = questions[1]
+    questionElem.textContent = questions[testvar]
     alltheButtons.setAttribute('style', 'display: flex')
+    choiceA.textContent = correctAnswers1[testvar]
+    choiceB.textContent = correctAnswers2[testvar]
+    choiceC.textContent = correctAnswers3[testvar]
+    choiceD.textContent = correctAnswers4[testvar]
+};
 
-    console.log('quiz started')
 
-}
+
+//event listener to begin quiz
+
+startButton.addEventListener('click', startQuiz)
+
+//button listeners to move to next question
 choiceA.addEventListener('click', function () {
     questionElem.textContent = questions[currentQuestion++]
     nextQ()
@@ -52,18 +67,9 @@ choiceD.addEventListener('click', function () {
 })
 
 
-if (correctAnswers.includes(selectedAnswer)){
-    correctcount++
-}
-    else{
-        wrongcount++
-        timeLeft -= 5
-    }
 
 
-//)
-
-// checks to see the game state and which header to display
+// checking game state to determine header
 function headerCheck() {
 
     if (headerElem.getAttribute('game-state') == 'pregame') {
@@ -74,23 +80,41 @@ function headerCheck() {
     }
     else if (headerElem.getAttribute('game-state') == 'postgame') {
         headerElem.textContent = "Game over! Let's see how you did"
+        alltheButtons.setAttribute('style', 'display: none')
+
     }
+}
+
+if(testvar == 5){
+gameOver()
+headerCheck()
 }
 
 
 //runs logic to load next question from array.
 function nextQ() {
-
-
+    testvar++
+    questionElem.textContent = questions[testvar]
+    choiceA.textContent = correctAnswers1[testvar]
+    choiceB.textContent = correctAnswers2[testvar]
+    choiceC.textContent = correctAnswers3[testvar]
+    choiceD.textContent = correctAnswers4[testvar]
     console.log("time for the next question")
 }
 
-function gameOver() {
-    headerElem.setAttribute('game-state', 'postgame')
 
+
+
+//runs page cleanup and starts the name entry and high score
+function gameOver() {
+    timeLeft = 0;
+    headerElem.setAttribute('game-state', 'postgame')
     console.log('the game has ended')
 }
 
+
+
+// timer function that starts when start button is pressed
 function timerGo() {
     let timerInterval = setInterval(function () {
         timeLeft--;
@@ -101,12 +125,12 @@ function timerGo() {
             clearInterval(timerInterval);
             timerCountEl.textContent = "Times up!"
             headerElem.setAttribute('game-state', 'postgame')
+            gameOver()
             headerCheck()
         }
 
     }, 1000)
 }
 
-startButton.addEventListener('click', startQuiz)
 
 
