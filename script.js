224ee0569520b46c07b8035fbc7977982976da05
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-23 14:37:17 -0700

    figured out the answer verification and score keeping.

--- script.js
+++ script.js
@@ -4,28 +4,31 @@ let correctAnswers2 = ['wrong', 'correct', 'a litte', 'a lottle', '42'];
 let correctAnswers3 = ['wrong', 'something else', 'correct', 'a lottle', '42'];
 let correctAnswers4 = ['wrong', 'something else', 'a litte', 'correct', '42'];
 let correctAnswers5 = ['wrong', 'something else', 'a litte', 'a lottle', 'correct'];
-let incorrectAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69'];
-let allAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69', 'something', 'something else', 'a litte', 'a lottle', '42']
+// let incorrectAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69'];
+let correctAnswers = ['correct','correct','a litte']
+// let allAnswers = ['123', '321', '111', '666', '420', '710', '911', '711', '411', '*69', 'something', 'something else', 'a litte', 'a lottle', '42']
 let startButton = document.querySelector("#pressme");
 let choiceA = document.querySelector("#a");
 let choiceB = document.querySelector("#b");
 let choiceC = document.querySelector("#c");
 let choiceD = document.querySelector("#d");
 let questionElem = document.querySelector("#question")
+let scoreElem = document.querySelector(".score1")
+let score2Elem = document.querySelector(".score2")
 let headerElem = document.querySelector('.main-title')
 headerElem.setAttribute('game-state', 'pregame')
 let correctcount = 0;
 let wrongcount = 0;
 let currentQuestion = 0;
-let timeLeft = 3;
+let timeLeft = 10;
 let timerCountEl = document.querySelector("#thetimer");
 let alltheButtons = document.querySelector('.choices');
 let selectedAnswer = '';
 let testvar = 0;
 let timerInterval = '';
 
-
 headerCheck();
+startButton.addEventListener('click', startQuiz)
 
 // starts quiz, changes header, displays first question
 
@@ -46,24 +49,67 @@ function startQuiz() {
 
 //event listener to begin quiz
 
-startButton.addEventListener('click', startQuiz)
+
 
 //button listeners to move to next question
 choiceA.addEventListener('click', function () {
+    if(correctAnswers.includes(event.target.innerText)){
+        correctcount++ 
+    }
+    else{wrongcount++
+        timeLeft-= 5}
     questionElem.textContent = questions[currentQuestion++]
+    if(testvar === 4){
+        gameOver()
+        }
+        console.log(event.target.innerText)
     nextQ()
 })
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 choiceB.addEventListener('click', function () {
+    if(correctAnswers.includes(event.target.innerText)){
+        correctcount++ 
+    }
+    else{wrongcount++
+        timeLeft-= 5}
     questionElem.textContent = questions[currentQuestion++]
+    if(testvar === 4){
+        gameOver()
+        }
+        console.log(event.target.innerText)
     nextQ()
 })
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 choiceC.addEventListener('click', function () {
+    if(correctAnswers.includes(event.target.innerText)){
+        correctcount++ 
+    }
+    else{wrongcount++
+    timeLeft-= 5}
     questionElem.textContent = questions[currentQuestion++]
+    if(testvar === 4){
+        gameOver()
+        }
+        console.log(event.target.innerText)
     nextQ()
 })
+
+
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 choiceD.addEventListener('click', function () {
+    if(correctAnswers.includes(event.target.innerText)){
+        correctcount++ 
+    }
+    else{wrongcount++
+    timeLeft-= 5}
     questionElem.textContent = questions[currentQuestion++]
+      console.log(event.target.innerTexts)
     nextQ()
+
 })
 
 
@@ -85,12 +131,6 @@ function headerCheck() {
     }
 }
 
-if(testvar == 5){
-gameOver()
-headerCheck()
-}
-
-
 //runs logic to load next question from array.
 function nextQ() {
     testvar++
@@ -100,27 +140,41 @@ function nextQ() {
     choiceC.textContent = correctAnswers3[testvar]
     choiceD.textContent = correctAnswers4[testvar]
     console.log("time for the next question")
+    if(testvar === 4){
+        gameOver()}
+        if(timeLeft <=0){
+
+            gameOver()
+        }
+        
 }
-
-
-
 
 //runs page cleanup and starts the name entry and high score
 function gameOver() {
-    timeLeft = 0;
     headerElem.setAttribute('game-state', 'postgame')
+    alltheButtons.setAttribute('style', 'display: none')
+    questionElem.setAttribute('style', 'display: none')
+    myStopFunction()
+    timerCountEl.textContent = ""
+    headerElem.setAttribute('game-state', 'postgame')
+    headerCheck()
     console.log('the game has ended')
+    highScore()
 }
 
 
+// function to stop timer, courtesy of w3schools. 
+function myStopFunction() {
+    clearInterval(timerInterval);
+  }
 
 // timer function that starts when start button is pressed
 function timerGo() {
-    let timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         timeLeft--;
         timerCountEl.textContent = timeLeft + " seconds remaining"
-        if (timeLeft == 1)
-            timerCountEl.textContent = timeLeft + " second remaining"
+        if (timeLeft == 1){
+            timerCountEl.textContent = timeLeft + " second remaining"}
         if (timeLeft == 0) {
             clearInterval(timerInterval);
             timerCountEl.textContent = "Times up!"
@@ -132,5 +186,10 @@ function timerGo() {
     }, 1000)
 }
 
+function highScore(){
+    scoreElem.textContent = "Correct: " + correctcount
+score2Elem.textContent = "incorrect: " + wrongcount
+
+}
 
 
