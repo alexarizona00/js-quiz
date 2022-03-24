b701b42ab93b0099bfaef4405617273bed9232a2
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-23 18:47:32 -0700

    final push with last changes

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # js-quiz
-hw regarding web apis
+
+
+<img width="956" alt="image" src="https://user-images.githubusercontent.com/82853759/159824742-8f7ddafe-4254-401d-a905-d8a62aba5760.png">
+
+https://github.com/alexarizona00/js-quiz
+
+https://alexarizona00.github.io/js-quiz/
+
+
+
 Hello!
 
-This is an interactive quiz we are building to test our javascript coding skills and others javascript quiz taking skills. 
+This is an interactive quiz we are building to test our javascript coding skills.
+
+I started off first with creating a basic layout in HTML with empty elements that I would populate using javascript DOM manipulation. 
+
+I then did some minor styling, and then focused on how I would get the quiz to work, specifically how it would collect the information contained within the button element when clicked and then how to compare that to what a correct answer should be. 
+
+Once i got the many variables, arrays and queryselectors setup ( with many additions/removals ) , I then worked on getting the button presses , regardless of which button was pressed. to move us through the questions/answers.
+
+I then worked on getting the score counted, added and combined for total score. I utilized a prompt to gather the users name and store it using .setItem in the local storage. 
+
+
+
--- script.js
+++ script.js
@@ -18,6 +18,7 @@ let headerElem = document.querySelector('.main-title')
 let scoreTable = document.querySelector('#scoretable')
 let tableName = document.querySelector('#scorename')
 let tableScore = document.querySelector('#scorenumber')
+let resetButton = document.querySelector('#reset')
 headerElem.setAttribute('game-state', 'pregame')
 let correctcount = 0;
 let wrongcount = 0;
@@ -214,6 +215,10 @@ function storeScore(){
     console.log(points)
     localStorage.setItem('username',userName)
     localStorage.setItem('score', points )
+    tableName.textContent = localStorage.getItem('username')
+    tableScore.textContent = localStorage.getItem('score')
+    // resetButton.setAttribute('style', 'display:block')
+
 
 }
 
@@ -224,74 +229,16 @@ scoreButt.addEventListener("click", function(){
     headerElem.setAttribute('style', 'display:none')
     userName = prompt("You have made the score list! Enter your name to find out if its THE high score!")
     scoreButt.setAttribute('style', 'display:none')
-    scoreTable.setAttribute('style', 'display:block')    
+    scoreTable.setAttribute('style', 'display:block') 
+    resetButton.setAttribute('style', "display:block")   
     storeScore() 
 
 return userName
-}
+})
+
+resetButton.addEventListener('click',function() {
+    location.reload()})
 
 
-)
-class Employee {
-    constructor(employee_username, password, department, yy, email, level) {
-      this.employee_user = employee_username;
-      this.password = password;
-      this.department = department;
-      this.email = email;
-      this.skills = [];
-      this.level = level;
-    }
-    
-    // function: Pushses new skill, in "Skills" array
-    addNewSkill(skill){
-      this.skills.push(skill);
-    }
-  }
-  
-  //Employee Database "Localstorage"
 
-  if(localStorage.getItem('Employee') == null) {
-     var employeeList = [];
-     employeeList.push (new Employee("Simon", 1234, "HR", 1999, "123@mail.dk", '1'));
-     employeeList.push (new Employee("Mads", 12345,"IT", 1999,  "1234@email.com", '1'));
-     employeeList.push (new Employee("Jessica", 54321, "Sales",1998, "Mail2@mail.dk",'1'));
-     employeeList.push (new Employee("Benjamin", 4321,"IT", 1997, "blabla@mail.dk", '1'));
-  
-     var employeeListString = JSON.stringify(employeeList)
-     localStorage.setItem('Employee', employeeListString)
-  } else {
-     var employeeList = JSON.parse(localStorage.getItem('Employee'))
-  }
-  
-  function buildTable(data) {
-    let table = document.createElement("table");
-    
-    // Create table head and body
-    table.appendChild(document.createElement('thead'));
-    table.appendChild(document.createElement('tbody'));
-    
-    let fields = Object.keys(data[0]);
-    let headRow = document.createElement("tr");
-    fields.forEach(function(field) {
-        let headCell = document.createElement("th");
-        headCell.textContent = field;
-        headRow.appendChild(headCell);
-    });
-    table.querySelector('thead').appendChild(headRow);
-    data.forEach(function(object) {
-        let row = document.createElement("tr");
-        fields.forEach(function(field) {
-            let cell = document.createElement("td");
-            cell.textContent = object[field];
-            if (typeof object[field] == "number") {
-                cell.style.textAlign = "right";
-            }
-            row.appendChild(cell);
-        });
-        table.querySelector('tbody').appendChild(row);
-    });
-    return table;
-  }
-  
-  document.querySelector('#employees').appendChild(buildTable(employeeList));
 
